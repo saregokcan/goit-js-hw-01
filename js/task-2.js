8c83a3614f8c2b5328f60639432e8628391a4e7f
saregokcan <gokcansare@gmail.com>
2025-04-13 00:29:52 +0300

    js-homework-finsh

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
-    return  'Shipping to $(country) will cost $(totalPrice) credits'
+    let totalPrice = price + deliveryFee;
+    return  `Shipping to ${country} will cost ${totalPrice} credits`
 }
 
 console.log(getShippingMessage("Australia", 120, 50));
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-
+return parseFloat(content) +  2*parseFloat(padding)+ 2*parseFloat(border);
 }
 
 
